439e8bc832ed2871be204d2f23d3cef4c9e88c4b
Josue Lubaki <josuelubaki@gmail.com>
2023-01-26 15:07:24 -0500

    Handling User Input & Background Image

--- App.js
+++ App.js
@@ -1,20 +1,27 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, ImageBackground } from 'react-native';
+import StartGameScreen from './screens/StartGameScreen';
+import { LinearGradient } from 'expo-linear-gradient';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<LinearGradient colors={['#4e0329', '#ddb52f']} style={styles.rootScreen}>
+			<ImageBackground
+				source={require('./assets/images/dices-img.png')}
+				resizeMode='cover'
+				style={styles.rootScreen}
+				imageStyle={styles.backgroundImage}
+			>
+				<StartGameScreen />
+			</ImageBackground>
+		</LinearGradient>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	rootScreen: {
+		flex: 1,
+	},
+	backgroundImage: {
+		opacity: 0.2,
+	},
 });
