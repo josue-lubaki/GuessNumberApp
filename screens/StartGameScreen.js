217f27175053801567dae540d46ce39929a3ad45
Josue Lubaki <josuelubaki@gmail.com>
2023-01-29 20:34:40 -0500

    managing Screen Content With KeyboardAvoidingView and ScrollView

--- screens/StartGameScreen.js
+++ screens/StartGameScreen.js
@@ -1,5 +1,13 @@
 import { useState } from 'react';
-import { Alert, StyleSheet, Text, TextInput, View } from 'react-native';
+import {
+	Alert,
+	StyleSheet,
+	TextInput,
+	View,
+	useWindowDimensions,
+	KeyboardAvoidingView,
+	ScrollView,
+} from 'react-native';
 import PrimaryButton from '../components/ui/PrimaryButton';
 import Title from '../components/ui/Title';
 import Card from '../components/ui/Card';
@@ -8,6 +16,9 @@ import Colors from '../constants/Colors';
 
 const StartGameScreen = ({ onPickedNumber }) => {
 	const [enteredNumber, setEnteredNumber] = useState('');
+
+	const { width, height } = useWindowDimensions();
+
 	const numberInputHandler = (enteredText) => {
 		setEnteredNumber(enteredText);
 	};
@@ -32,37 +43,45 @@ const StartGameScreen = ({ onPickedNumber }) => {
 		onPickedNumber(chosenNumber);
 	};
 
+	const marginTopDistance = height < 380 ? 30 : 100;
+
 	return (
-		<View style={styles.rootContainer}>
-			<Title>Guess My Number</Title>
-			<Card>
-				<InstructorText>Enter a Number</InstructorText>
-				<TextInput
-					style={styles.numberInput}
-					maxLength={2}
-					keyboardType='number-pad' // https://reactnative.dev/docs/textinput#keyboardtype
-					autoCapitalize='none' // https://reactnative.dev/docs/textinput#autocapitalize
-					autoCorrect={false} // https://reactnative.dev/docs/textinput#autocorrect
-					onChangeText={numberInputHandler}
-					value={enteredNumber}
-				/>
-				<View style={styles.buttonsContainer}>
-					<View style={styles.buttonContainer}>
-						<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
-					</View>
-					<View style={styles.buttonContainer}>
-						<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
-					</View>
+		<ScrollView style={styles.screen}>
+			<KeyboardAvoidingView style={styles.screen} behavior='position'>
+				<View style={[styles.rootContainer, { marginTop: marginTopDistance }]}>
+					<Title>Guess My Number</Title>
+					<Card>
+						<InstructorText>Enter a Number</InstructorText>
+						<TextInput
+							style={styles.numberInput}
+							maxLength={2}
+							keyboardType='number-pad' // https://reactnative.dev/docs/textinput#keyboardtype
+							autoCapitalize='none' // https://reactnative.dev/docs/textinput#autocapitalize
+							autoCorrect={false} // https://reactnative.dev/docs/textinput#autocorrect
+							onChangeText={numberInputHandler}
+							value={enteredNumber}
+						/>
+						<View style={styles.buttonsContainer}>
+							<View style={styles.buttonContainer}>
+								<PrimaryButton onPress={resetInputHandler}>Reset</PrimaryButton>
+							</View>
+							<View style={styles.buttonContainer}>
+								<PrimaryButton onPress={confirmInputHandler}>Confirm</PrimaryButton>
+							</View>
+						</View>
+					</Card>
 				</View>
-			</Card>
-		</View>
+			</KeyboardAvoidingView>
+		</ScrollView>
 	);
 };
 
 const styles = StyleSheet.create({
+	screen: {
+		flex: 1,
+	},
 	rootContainer: {
 		flex: 1,
-		marginTop: 100,
 		alignItems: 'center',
 	},
 	numberInput: {
